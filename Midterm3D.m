0240c2124e07900d0562582aee7c56f6c6bf57ba
HUAN-CHE HSU <160444147+HuanCheHsu@users.noreply.github.com>
2024-06-19 22:05:38 +0800

    Add files via upload

---
+++ Midterm3D.m
@@ -0,0 +1,5 @@
+%Mid term question 3 3D space
+x=input('Enter value for coordinate x: ');
+y=input('Enter value for coordinate y: ');
+z=input('Enter value for coordinate z: ');
+printspherical(x,y,z);
